6b7f872d4eef5818f31a427a77994eb316028a20
María Jesús Senosiain Caamiña <mjsenosiainc@gmail.com>
2015-08-12 22:27:48 +0200

    - Create class MJSCCoreDataStack and delete Core Data stuff from AppDelegate.

---
+++ Library/MJSCCoreDataStack.h
@@ -0,0 +1,24 @@
+//
+//  MJSCCoreDataStack.h
+//  Library
+//
+//  Created by María Jesús Senosiain Caamiña on 12/08/15.
+//  Copyright (c) 2015 María Jesús Senosiain Caamiña. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface MJSCCoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++(MJSCCoreDataStack *)sharedInstance;
+
+- (instancetype)init __attribute__((unavailable("init not available, use shareInstace...")));
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
